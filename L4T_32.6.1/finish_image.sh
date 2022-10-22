8b19f70465e1f75a8c4c082bb74fdd999541f8a2
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-10-22 14:33:14 -0700

    checkout source code

--- L4T_32.6.1/finish_image.sh
+++ L4T_32.6.1/finish_image.sh
@@ -11,11 +11,31 @@ chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get update"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install dnsutils minicom nano iperf iperf3 netcat-openbsd"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install ethtool net-tools"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install i2c-tools"
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install lbzip pigz"
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install rsync wget"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install v4l-utils"
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install ca-certificates"
+
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install build-essential cmake git"
 
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "adduser mbari dialout"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "adduser mbari i2c"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "adduser mbari gpio"
+
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gtk-doc-tools"
+chroot --userspec=mbari:mbari /home/buildbot/Linux_for_Tegra/rootfs/home/mbari/gst-interpipe /bin/bash -c "git clone https://github.com/RidgeRun/gst-interpipe.git"
+chroot --userspec=mbari:mbari /home/buildbot/Linux_for_Tegra/rootfs/home/mbari/gst-interpipe /bin/bash -c "git checkout v1.1.8"
+chroot --userspec=mbari:mbari /home/buildbot/Linux_for_Tegra/rootfs/home/mbari/gst-interpipe /bin/bash -c "./autogen.sh --libdir /usr/lib/aarch64-linux-gnu/"
+chroot --userspec=mbari:mbari /home/buildbot/Linux_for_Tegra/rootfs/home/mbari/gst-interpipe /bin/bash -c "make"
+chroot --userspec=mbari:mbari /home/buildbot/Linux_for_Tegra/rootfs/home/mbari/gst-interpipe /bin/bash -c "make check"
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari/gst-interpipe /bin/bash -c "make install"
+
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get -y install libboost-all-dev"
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get -y install libgstreamermm-1.0-dev libgstrtspserver-1.0-dev"
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get -y install libopencv-imgproc-dev libopencv-highgui-dev libopencv-imgcodecs-dev libopencv-core-dev"
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get -y install googletest google-mock"
+rapidjson-dev
+chroot --userspec=mbari:mbari /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "git clone https://github.com/suburbanmarine/opaleye.git"
 
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "mv /etc/resolv.conf.bak /etc/resolv.conf"
 popd
