b51f116a07a0f6d7b6b4d288b6e4631ed656b821
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2022-11-09 21:37:21 -0800

    build diff

--- L4T_32.6.1/finish_image.sh
+++ L4T_32.6.1/finish_image.sh
@@ -17,11 +17,12 @@ chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install dn
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install ethtool net-tools"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install i2c-tools"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install lbzip pigz"
-chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install rsync wget"
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install rsync wget gdebi"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install v4l-utils"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install ca-certificates"
 
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install build-essential cmake git"
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install gtk-doc-tools"
 
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install libboost-all-dev libelf-dev"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install libuvc-dev"
@@ -32,20 +33,29 @@ chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install li
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install googletest google-mock"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "apt-get -y install libprotobuf-dev libprotobuf-c-dev libprotoc-dev protobuf-compiler protobuf-c-compiler protobuf-compiler-grpc libgrpc-dev libgrpc++-dev"
 
-chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gtk-doc-tools"
-chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari && git clone https://github.com/RidgeRun/gst-interpipe.git"
-chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/gst-interpipe && git checkout v1.1.8"
+# fix module link
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "rm /lib/modules/4.9.253-tegra/build"
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "ln -s /usr/src/linux-headers-4.9.253-tegra-ubuntu18.04_aarch64/kernel-4.9 /lib/modules/4.9.253-tegra/build"
+
+chroot /home/buildbot/Linux_for_Tegra/rootfs/home/mbari /bin/bash -c "apt-get -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gtk-doc-tools"
+chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari && export HOME=/home/mbari && git clone https://github.com/RidgeRun/gst-interpipe.git"
+chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/gst-interpipe && export HOME=/home/mbari && git checkout v1.1.8"
 chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/gst-interpipe && ./autogen.sh --libdir /usr/lib/aarch64-linux-gnu/"
 chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/gst-interpipe && make -j`nproc`"
 chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/gst-interpipe && make check"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/gst-interpipe && make install"
 
-chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari     && git clone https://github.com/ZeroCM/zcm.git"
+chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari     && export HOME=/home/mbari && git clone https://github.com/ZeroCM/zcm.git"
 chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/zcm && ./waf configure --use-inproc --use-ipc --use-udpm --use-zmq --use-elf"
 chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/zcm && ./waf build -j`nproc`"
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/zcm && ./waf install"
 
-chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari && git clone https://github.com/suburbanmarine/opaleye.git"
+chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari && export HOME=/home/mbari && git clone https://github.com/suburbanmarine/opaleye.git"
+chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/opaleye && export HOME=/home/mbari && git submodule init"
+chroot --userspec=+1000:+1000 /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "cd /home/mbari/opaleye && export HOME=/home/mbari && git submodule update"
 
 chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c "mv /etc/resolv.conf.bak /etc/resolv.conf"
+
+chroot /home/buildbot/Linux_for_Tegra/rootfs /bin/bash -c  "rm /etc/apt/apt.conf.d/02proxy"
+
 popd
